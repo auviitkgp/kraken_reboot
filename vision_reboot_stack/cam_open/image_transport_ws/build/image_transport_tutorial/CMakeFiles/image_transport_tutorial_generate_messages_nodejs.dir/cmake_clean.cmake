bc3607a471a936b1f817b971966acdfbf5c3c68c
sangeet259 <mail2sangeetmishra@gmail.com>
2018-03-03 09:12:43 +0530

    adds the camera opening node

---
+++ vision_reboot_stack/cam_open/image_transport_ws/build/image_transport_tutorial/CMakeFiles/image_transport_tutorial_generate_messages_nodejs.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/image_transport_tutorial_generate_messages_nodejs"
+  "/home/sangeet/image_transport_ws/devel/share/gennodejs/ros/image_transport_tutorial/msg/ResizedImage.js"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/image_transport_tutorial_generate_messages_nodejs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
