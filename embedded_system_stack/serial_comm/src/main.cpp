ce8712b6f561ee1a284a62a395774bace8b17538
yashagarwal9 <yashaga98@gmail.com>
2018-05-25 21:18:25 +0530

    Adding Serial_comm for Serial communication

---
+++ embedded_system_stack/serial_comm/src/main.cpp
@@ -0,0 +1,45 @@
+#include "ros/ros.h"
+#include "string.h"
+#include "serial/serial.h"
+#include "kraken_msgs/thrusterCmd.h"
+#include "resources/topicHeader.h"
+
+void commCB(const kraken_msgs::thrusterCmdConstPtr &msg);
+uint8_t data[18];
+
+int main(int argc, char **argv){
+  ros::init(argc, argv, "serial_comm_node");
+  if(argc < 2){
+    ROS_INFO("BAUDRATE OR PORT NOT SPECIFIED");
+    return 0;
+  }
+  std::string port(argv[2]);
+  uint32_t baud;
+  sscanf(argv[1], "%u", &baud);
+  ros::NodeHandle n;
+  ros::Subscriber CmdSub = n.subscribe<kraken_msgs::thrusterCmd>(topics::CONTROL_SEABOTIX, 1000, commCB);
+  serial::Serial comm(port, baud, serial::Timeout::simpleTimeout(1000));
+  ros::Rate loop_rate(8);
+  while(ros::ok()){
+    if(comm.isOpen()) ROS_INFO("SERIAL PORT IS OPEN");
+    else{
+      comm.open();
+      ROS_INFO("SERIAL PORT IS OPENED");
+    }
+    size_t bytes_written = comm.write(data, 18);
+    std::cout << "BYTES SEND: "<<bytes_written<< "\n";
+    comm.close();
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+  return 0;
+}
+
+void commCB(const kraken_msgs::thrusterCmd::ConstPtr &msg){
+  for(int i = 0; i<6 ; i++){
+    data[3*i+0] = msg->data_array[i].thruster_id;
+    data[3*i+1] = msg->data_array[i].speed;
+    data[3*i+2] = msg->data_array[i].info;
+  }
+  std::cout <<"SUCCESS\n";
+}
