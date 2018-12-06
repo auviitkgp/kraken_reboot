f83d916edf832cdefd17ad3e21fcee82584841bb
ds <ae>
2018-12-06 20:50:15 +0530

    Changes made for dynamic_reconfigure in main.cpp

--- control_system_stack/control_system/src/main.cpp
+++ control_system_stack/control_system/src/main.cpp
@@ -23,16 +23,16 @@ int main(int argc, char **argv){
         actionlib::SimpleActionServer<kraken_msgs::advancedControllerAction> _ControlServer(n, topics::CONTROL_ADVANCEDCONTROLLER_ACTION, boost::bind(&kraken_controller::ControlServer::executeGoalCB, &_server, _1), false);
         _server.setServer(&_ControlServer);
 
-        dynamic_reconfigure::Server<control_system::paramsConfig> surge(n_a);
+        /*dynamic_reconfigure::Server<control_system::paramsConfig> surge(n_a);
         dynamic_reconfigure::Server<control_system::paramsConfig> depth(n_b);
         dynamic_reconfigure::Server<control_system::paramsConfig> yaw(n_c);
         dynamic_reconfigure::Server<control_system::paramsConfig> pitch(n_d);
-        
+
         surge.setCallback(boost::bind(&kraken_controller::ControlServer::callback0, &_server, _1, _2));
         depth.setCallback(boost::bind(&kraken_controller::ControlServer::callback1, &_server, _1, _2));
         yaw.setCallback(boost::bind(&kraken_controller::ControlServer::callback2, &_server, _1, _2));
         pitch.setCallback(boost::bind(&kraken_controller::ControlServer::callback3, &_server, _1, _2));
-        
+        */
         ros::spin();
     }
     else std::cerr<<"Server 'file1' 'file2' .... "<<std::endl;
