dd9dda4ad596cc8423033fd61736d590b8248aec
yashagarwal9 <yashaga98@gmail.com>
2019-01-13 02:15:25 +0530

    Merge conflit removed

--- control_system_stack/control_system/src/main.cpp
+++ control_system_stack/control_system/src/main.cpp
@@ -22,24 +22,7 @@ int main(int argc, char **argv){
         _server.loadParams(_files);
         actionlib::SimpleActionServer<kraken_msgs::advancedControllerAction> _ControlServer(n, topics::CONTROL_ADVANCEDCONTROLLER_ACTION, boost::bind(&kraken_controller::ControlServer::executeGoalCB, &_server, _1), false);
         _server.setServer(&_ControlServer);
-<<<<<<< HEAD
-	//if(argv[argc-1] == std::string("TUNE")){
-        dynamic_reconfigure::Server<control_system::paramsConfig> thruster0(n_a);
-        dynamic_reconfigure::Server<control_system::paramsConfig> thruster1(n_b);
-        // dynamic_reconfigure::Server<control_system::paramsConfig> thruster2(n_c);
-        // dynamic_reconfigure::Server<control_system::paramsConfig> thruster3(n_d);
-        // dynamic_reconfigure::Server<control_system::paramsConfig> thruster4(n_e);
-        // dynamic_reconfigure::Server<control_system::paramsConfig> thruster5(n_f);
-        thruster0.setCallback(boost::bind(&kraken_controller::ControlServer::callback0, &_server, _1, _2));
-        thruster1.setCallback(boost::bind(&kraken_controller::ControlServer::callback1, &_server, _1, _2));
-        // thruster2.setCallback(boost::bind(&kraken_controller::ControlServer::callback2, &_server, _1, _2));
-        // thruster3.setCallback(boost::bind(&kraken_controller::ControlServer::callback3, &_server, _1, _2));
-        // thruster4.setCallback(boost::bind(&kraken_controller::ControlServer::callback4, &_server, _1, _2));
-        // thruster5.setCallback(boost::bind(&kraken_controller::ControlServer::callback5, &_server, _1, _2));
-	//}
-=======
-
-        /*dynamic_reconfigure::Server<control_system::paramsConfig> surge(n_a);
+        dynamic_reconfigure::Server<control_system::paramsConfig> surge(n_a);
         dynamic_reconfigure::Server<control_system::paramsConfig> depth(n_b);
         dynamic_reconfigure::Server<control_system::paramsConfig> yaw(n_c);
         dynamic_reconfigure::Server<control_system::paramsConfig> pitch(n_d);
@@ -48,8 +31,6 @@ int main(int argc, char **argv){
         depth.setCallback(boost::bind(&kraken_controller::ControlServer::callback1, &_server, _1, _2));
         yaw.setCallback(boost::bind(&kraken_controller::ControlServer::callback2, &_server, _1, _2));
         pitch.setCallback(boost::bind(&kraken_controller::ControlServer::callback3, &_server, _1, _2));
-        */
->>>>>>> yash/testing-controls
         ros::spin();
     }
     else std::cerr<<"Server 'file1' 'file2' .... "<<std::endl;
